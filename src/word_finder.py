a85084a5c6a00bc782b4a08880c0268075fe20f1
pedro-parno <pedroparno@gmail.com>
2024-06-03 23:41:43 -0300

    fix -> commented wrong implementation and adding n to divide content in lines

--- src/word_finder.py
+++ src/word_finder.py
@@ -4,15 +4,15 @@ def lines_with_word_occurrences(file_path: str, word: str):
     for index, line in enumerate(str_lines, start=1):
         if word.casefold() in line.casefold():
             result.append(index)
-        else:
-            str_lines.remove(line)
+    # else:
+    #     str_lines.remove(line)
     return result
 
 
 def read_file_lines(file_path):
     with open(file_path, "r") as file:
         file_content = file.read()
-    return file_content.split()
+    return file_content.split("\n")
 
 
 def main():
